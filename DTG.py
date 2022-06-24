08e0f9325ad33ab788e09f3aba9045628a496151
benekker <benekker1@gmail.com>
2022-06-24 14:55:26 -0500

    updated DTG progress

--- DTG.py
+++ DTG.py
@@ -29,32 +29,65 @@ def get_destination():
     print(f'We have chosen {destination_choice} as your destination.')
     return destination_choice
 get_destination()
+
 def get_restaurant():
     restaraunt_choice = random.choice(restaurants)
     print(f'We have chosen {restaraunt_choice} as your restaurant.')
     return restaraunt_choice
 get_restaurant()
+
 def get_transportation():
     transportation_choice = random.choice(transportation)
     print(f'We have chosen {transportation_choice} as your method of transport.')
     return transportation_choice
 get_transportation()
+
 def get_entertainment():
     entertainment_choice = random.choice(entertainment)
     print(f'And, we have chosen {entertainment_choice} as your entertainment option!')
     return entertainment_choice
 get_entertainment()
+
 def get_user_opinion():
     print('Are you happy with all of your results?')
     opinion = input('yes or no? ')
     return opinion
 result = get_user_opinion()
+
 def settle_opinion(response):
     if response == 'no':
         print("Oh no! We're sorry you're not happy with your results. No problem, let's change that!")
     elif response == 'yes':
         print("Perfect! we're happy that you're happy!")
 settle_opinion(result)
+
+def get_change(response):
+    if response == 'no':
+        print('What would you like to change?')
+        change = input ("enter '1' for destination, enter '2' for restaurant, enter '3' for transport, and enter '4' for entertainment: ")
+        return change
+change_result = get_change(result)
+
+def get_rerun(response):
+    new_destination_choice = random.choice(destinations)
+    cont_loop = True
+    while cont_loop == True:
+        if change_result == '1':
+            print(f'How about {new_destination_choice} as your destination? ')
+            decision = input('yes or no? ')
+        if decision == 'yes':
+            cont_loop = False
+        else:
+            cont_loop = True
+get_rerun(change_result)
+
+
+
+    
+
+
+
+
 
 
 
